314ba44b236fdb0ceca961ba2320dbafe594c32b
Damir <ag_cmg@mail.ru>
2023-02-21 18:55:01 +0300

    zad36

---
+++ zadacha_36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] array = GetArray(10, -99, 100);
+Console.WriteLine(String.Join(" ", array));
+
+int result(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i+2)
+    {
+        sum += array[i];
+    }
+    return sum ;
+
+}
+
+int sum = result(array);
+Console.WriteLine(sum);
